d1d115da19660fab5f54ffda1be2d61e4baf2b63
alish1999 <62278628+alish1999@users.noreply.github.com>
2022-01-06 16:35:35 +0530

    Add files via upload

---
+++ my-mysql/sql-scripts/InsertData.sql
@@ -0,0 +1,2 @@
+insert into studentData (Name,Roll_no,Addres,Mobile,Pan)
+VALUES ('Alish Bhosale',10,"railwaylines","9090909090","90292929");
---
+++ my-mysql/sql-scripts/test.sql
@@ -0,0 +1,7 @@
+Create table studentData(
+  Name VARCHAR(25),
+  Roll_no INT,
+  Addres VARCHAR(25),
+  Mobile VARCHAR(15),
+  Pan VARCHAR(15)
+  );
